c3f1f036669086eedb502745d19d59c2e9af4738
Kevin Saforo <saforok@mymacewan.ca>
2016-11-13 08:23:48 -0700

    changes

--- Android/RadarDetector/app/src/main/java/com/vortecs/reboss/radardetector/MainActivity.java
+++ Android/RadarDetector/app/src/main/java/com/vortecs/reboss/radardetector/MainActivity.java
@@ -2,9 +2,12 @@ package com.vortecs.reboss.radardetector;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,7 +30,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.lang.Math;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager locationManager;
 
     private final String SERVER = "http://hacked2016.herokuapp.com";
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -46,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Emitter.Listener onNewFence = new Emitter.Listener() {
-
         @Override
         public void call(final Object... args) {
             runOnUiThread(new Runnable() {
@@ -54,10 +57,14 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     JSONObject data = (JSONObject) args[0];
 
+                    Log.d(TAG, data.toString());
+
                 }
             });
         }
+
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,9 +77,24 @@ public class MainActivity extends AppCompatActivity {
         JDRIVE.instance().initialize(getFilesDir().getPath());
         JDRIVE.instance().setLocationProvider(gpxLocation);
 
-        mSocket.on("new fence", onNewFence);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
+
+        mSocket.on("push data", onNewFence);
         mSocket.connect();
-        //JDRIVE.instance().run();
+
+        JDRIVE.instance().run();
     }
 
     private String GetTheOSUData() {
@@ -83,8 +105,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setPhotoRadar(View view) {
-        //double[]coordinates = getCoordinates();
-        final double[] coordinates = {49.202011, 113.020292};
+        double[]coordinates = getCoordinates();
+       //final double[] coordinates = {49.202011, 113.020292};
 
         Thread thread = new Thread(new Runnable() {
             @Override
@@ -160,9 +182,10 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
         if (GpsPermission) {
-            locationManager.requestLocationUpdates(
+            /*locationManager.requestLocationUpdates(
                     LocationManager.GPS_PROVIDER, 5000, 10,
-                    (LocationListener) locationListener);
+                    (LocationListener) locationListener); */
+
             double[] coord = {myLocation.getLat(), myLocation.getLong()};
             return coord;
         }
@@ -256,5 +279,32 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "Lat = " + locx[i][0] + " Long = " + locx[i][1]);
         }
         return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String msg = "New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
